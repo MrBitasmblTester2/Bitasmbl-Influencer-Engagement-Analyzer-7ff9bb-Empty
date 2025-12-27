acea90e800e26b765850fe7af7069ca385e67d0c
MrBitasmblTester2 <lulu2214@proton.me>
2025-12-27 20:49:50 +0400

    Add _app.tsx

---
+++ frontend/pages/_app.tsx
@@ -0,0 +1,1 @@
+import {ApolloClient,InMemoryCache,ApolloProvider} from '@apollo/client';import type {AppProps} from 'next/app';const client=new ApolloClient({uri:'http://localhost:4000',cache:new InMemoryCache()});export default function App({Component,pageProps}:AppProps){return(<ApolloProvider client={client}><Component {...pageProps}/></ApolloProvider>);} 
